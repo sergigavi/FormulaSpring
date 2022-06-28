4329b806ba1f3bdb14601d65424e82d3b032164b
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-06-28 14:31:30 +0200

    Update db1.sql

--- ddbb/db1.sql
+++ ddbb/db1.sql
@@ -1,4 +1,4 @@
 create database if not exists formulaSpring;
 
-create user 'sergioAdmin'@'%' identified by '123abc'; -- Creates the user
-grant all on formulaSpring.* to 'sergioAdmin'@'%'; -- Gives all privileges to the new user on the newly created database
+#create user 'sergioAdmin'@'%' identified by '123abc'; -- Creates the user
+#grant all on formulaSpring.* to 'sergioAdmin'@'%'; -- Gives all privileges to the new user on the newly created database
