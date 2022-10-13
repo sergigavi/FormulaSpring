07c7cbaf869cb3152228e9f6a111c661172fa277
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-10-13 18:07:59 +0200

    Añado cliente web javascript

---
+++ MiClienteWeb/index.js
@@ -0,0 +1,16 @@
+const index = document.querySelector("#miApp");    //. para clase / # para id
+
+const URL_API = "http://127.0.1.1:8080/FormulaSpring";
+
+fetch(`${URL_API}/escuderias/dametodas`)
+.then((res) => res.json())
+.then((escuderias) => {
+
+    console.log(escuderias)
+    
+    const divLista = escuderias.map((escuderia) => `<li>${escuderia.id} 🏎 ${escuderia.nombre}</li>`)
+
+    index.innerHTML = `${divLista}`
+
+})
+.catch;
--- src/main/java/es/sgv/FIA/restControllers/EscuderiasRestController.java
+++ src/main/java/es/sgv/FIA/restControllers/EscuderiasRestController.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import es.sgv.FIA.services.IEscuderiaService;
 import es.sgv.FIA.services.IPilotoService;
 import es.sgv.FIA.services.ITrabajadorService;
 
+@CrossOrigin	//	Esto permite que se acceda a la API saltandose el CORS (permite el fetch en javascript por ejemplo)	//https://spring.io/guides/gs/rest-service-cors/
 @RestController
 @RequestMapping("/FormulaSpring/escuderias")
 public class EscuderiasRestController {
